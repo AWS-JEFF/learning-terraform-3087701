6b7c3efd8a0702505ca34331ed42c4b3214d2f20
AWS-JEFF <104709792+AWS-JEFF@users.noreply.github.com>
2023-01-05 16:04:23 +0800

    create dev vpc

--- main.tf
+++ main.tf
@@ -46,12 +46,13 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.2"
-  name = "blog_new"
   
+  name = "blog"
   vpc_id = module.vpc.public_subnets[0]
+  
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules = ["all-all"]
-  egress_cidr_rules = ["0.0.0.0/0"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
